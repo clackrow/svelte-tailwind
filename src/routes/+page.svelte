b78fdbe4edd39d711588ecdf1a019b72ef90825a
Rodrigo <guigoazzolini@gmail.com>
2023-10-12 18:22:28 -0300

    first

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<div id="bg" class="h-screen bg-gradient-to-b from from-blue-300 to-cyan-600 p-1">
+    <div class="bg-gradient-to-r from-purple-700 to-blue-600 w-1/2 mx-auto p-10 rounded-full mt-40 text-center">
+        <h1 class="text-2xl font-bold">Welcome to SvelteKit</h1>
+        <p>Visit <a href="https://kit.svelte.dev" class="underline text-blue-200">kit.svelte.dev</a> to read the documentation</p>
+    </div>
+</div>
+
+
